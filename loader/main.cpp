840d6685167b5f4447a05711cb6394de48d9de50
namreeb <github@namreeb.org>
2017-02-22 18:16:25 -1000

    Update copyright year

--- loader/main.cpp
+++ loader/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2015, namreeb (legal@namreeb.org)
+    Copyright (c) 2017, namreeb (legal@namreeb.org)
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
